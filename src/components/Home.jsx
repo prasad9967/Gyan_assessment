fd96d701c3473582eb1938726006c694f8df888d
prasad9967 <gubbaladurgaprasad25@gmail.com>
2024-04-05 17:22:00 +0530

    ksldf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import BannerImage from '../assets/Banner.svg'
 
 const Home = () => {
   return (
     <div className='m-2 mt-4 mb-0 relative'>
-      <img src="./src/assets/Banner.svg" alt="Banner" />
+      <img src={BannerImage} alt="Banner" />
         <div className='absolute lg:top-[10rem] top-[1rem] text-gray-50'>
             <h1 className='lg:text-[3rem] text-[1rem] font-semibold w-2/3 mx-auto'>Discover Exciting Events Happening Near You - Stay Tuned for Updates!</h1>
             <p className='w-4/5 mx-auto text-[0.6rem] lg:text-xl mt-2'>Dorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu and litora torquent per conubia nostra, per</p>
